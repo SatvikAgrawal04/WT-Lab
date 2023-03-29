9889d0d4b13dfdb22be5ffba12a33611027e2863
Satvik Agrawal <satwikagrawal@gmail.com>
2023-03-29 13:44:41 +0530

    29/3/23

--- Lab10/q5.java
+++ Lab10/q5.java
@@ -2,17 +2,28 @@ package Lab10;
 
 import java.util.Scanner;
 
+class NegativeNumberException extends Exception{
+    NegativeNumberException(String str){
+        System.out.println(str);
+    }
+}
+
 class InputException{
     void processInput(){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        if (a < 0){
-            throw NegativeNumberException;
-        }
+        try{
+            if (a < 0){
+                throw new NegativeNumberException("Negative value Entered");
+            }
+
+            System.out.println(2*a);
+        }catch (NegativeNumberException m){}
     }
 }
 public class q5 {
     public static void main(String[] args) {
-
+        InputException obj = new InputException();
+        obj.processInput();
     }
 }
